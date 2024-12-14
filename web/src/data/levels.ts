0bc021454daefce3c93de01d0748e5aac64949e3
Saroj <sarojregmi.official@gmail.com>
2024-12-14 13:26:59 +0545

    chore: updated levels and their descriptions

--- web/src/data/levels.ts
+++ web/src/data/levels.ts
@@ -21,53 +21,53 @@ export type tlevelInfo = typeof LEVELS[tlevelKey]
 
 export const LEVELS = Object.freeze({
     1: {
-        name: "Level 1",
-        description: "Navigate the bustling streets of Jakarta, where ancient treasures hide among modern skyscrapers.",
+        name: "Perfect Information",
+        description: "Follow the rules.",
         location: LEVELS_TO_LOCATION_MAP[1]
     },
     2: {
-        name: "Level 2",
-        description: "Explore Singapore's vibrant cityscape, where tradition meets innovation in a garden city.",
+        name: "Dominant Strategy",
+        description: "Accumulate wealth to show dominance.",
         location: LEVELS_TO_LOCATION_MAP[2]
     },
     3: {
-        name: "Level 3",
-        description: "Journey through Ho Chi Minh City's maze of streets, uncovering secrets in this historic metropolis.",
+        name: "Zero sum game",
+        description: "Minimize loss.",
         location: LEVELS_TO_LOCATION_MAP[3]
     },
     4: {
-        name: "Level 4",
-        description: "Venture into Thailand's ancient ruins, where mystical temples guard age-old mysteries.",
+        name: "Prisoner's dilemma",
+        description: "Develop a breakthrough strategy to manage the change. Put yourself in others shoes and act accordingly.",
         location: LEVELS_TO_LOCATION_MAP[4]
     },
     5: {
-        name: "Level 5",
-        description: "Trek through Laos' misty mountains, discovering hidden villages and forgotten paths.",
+        name: "Imperfect Information",
+        description: "Validate your perception about other players.",
         location: LEVELS_TO_LOCATION_MAP[5]
     },
     6: {
-        name: "Level 6",
-        description: "Uncover the secrets of Myanmar's golden temples and sacred sites in Bagan.",
+        name: "Game of chicken",
+        description: "Determine your risk appetite.",
         location: LEVELS_TO_LOCATION_MAP[6]
     },
     7: {
-        name: "Level 7",
-        description: "Scale the heights of Yunnan's dramatic landscapes, where nature meets ancient Chinese culture.",
+        name: "Variable sum",
+        description: "Break away from the pact.",
         location: LEVELS_TO_LOCATION_MAP[7]
     },
     8: {
-        name: "Level 8",
-        description: "Cross the mystical valleys of Bhutan, where dragons and legends come alive.",
+        name: "Stag hunt",
+        description: "To be discussed.",
         location: LEVELS_TO_LOCATION_MAP[8]
     },
     9: {
-        name: "Level 9",
-        description: "Brave the Tibetan plateau's challenging terrain, where wisdom echoes through mountain passes.",
+        name: "Nash Equilibrium",
+        description: "Close in on victory.",
         location: LEVELS_TO_LOCATION_MAP[9]
     },
     10: {
-        name: "Level 10",
-        description: "Conquer the peaks of Nepal, where your journey reaches new heights in the shadow of Everest.",
+        name: "Mixed Strategy",
+        description: "Be Unpredictable.",
         location: LEVELS_TO_LOCATION_MAP[10]
     }
 } as const);
